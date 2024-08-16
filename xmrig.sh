563949163722ebb04048b1c6cab54a1d165801e2
Syndicate <100962424+Bravno@users.noreply.github.com>
2024-08-16 04:42:13 +0300

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -8,34 +8,42 @@ SCRIPT_PATH="$SCRIPT_DIR/$SCRIPT_NAME"
 # Установка прав на выполнение для самого скрипта
 chmod +x "$SCRIPT_PATH"
 
-# Обновление и установка необходимых пакетов
-if [ -f /etc/redhat-release ]; then
-    # Для Red Hat/CentOS
-    sudo yum update -y
-    sudo yum groupinstall -y "Development Tools"
-    sudo yum install -y cmake hwloc-devel libuv-devel
-else
-    # Для Debian/Ubuntu
-    sudo apt update
-    sudo apt install -y cmake build-essential libhwloc-dev libuv1-dev python3-pip
-fi
+# Функция для установки зависимостей
+install_dependencies() {
+    if [ -f /etc/redhat-release ]; then
+        # Для Red Hat/CentOS
+        sudo yum update -y
+        sudo yum groupinstall -y "Development Tools"
+        sudo yum install -y cmake hwloc-devel libuv-devel
+    else
+        # Для Debian/Ubuntu
+        sudo apt update
+        sudo apt install -y cmake build-essential libhwloc-dev libuv1-dev python3-pip
+    fi
+}
 
-# Установка Python-пакетов
-pip3 install --upgrade pip
-pip3 install requests
+# Функция для установки Python-пакетов
+install_python_packages() {
+    pip3 install --upgrade pip
+    pip3 install requests
+}
 
-# Клонирование репозитория XMRig
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
+# Функция для клонирования и сборки XMRig
+build_xmrig() {
+    # Клонирование репозитория XMRig
+    git clone https://github.com/xmrig/xmrig.git
+    cd xmrig || { echo "Не удалось перейти в директорию xmrig"; exit 1; }
 
-# Создание директории для сборки и сборка XMRig
-mkdir build
-cd build
-cmake ..
-make
+    # Создание директории для сборки и сборка XMRig
+    mkdir -p build
+    cd build || { echo "Не удалось перейти в директорию build"; exit 1; }
+    cmake .. || { echo "Ошибка конфигурации CMake"; exit 1; }
+    make || { echo "Ошибка сборки XMRig"; exit 1; }
+}
 
-# Создание конфигурационного файла config.json
-cat <<EOL > config.json
+# Функция для создания конфигурационного файла
+create_config_file() {
+    cat <<EOL > config.json
 {
   "autosave": true,
   "cpu": true,
@@ -53,10 +61,25 @@ cat <<EOL > config.json
   }
 }
 EOL
+}
 
-# Создание файла лога
-LOGFILE="/root/Xmrig_Log/xmrig/build/xmrig.log"
-touch $LOGFILE
+# Функция для создания файла лога и запуска XMRig
+run_xmrig() {
+    LOGFILE="/root/Xmrig_Log/xmrig/build/xmrig.log"
+    
+    # Создание директории для файла лога
+    mkdir -p "$(dirname "$LOGFILE")" || { echo "Не удалось создать директорию для файла лога"; exit 1; }
+    
+    # Создание файла лога
+    touch "$LOGFILE"
 
-# Запуск XMRig и логирование в реальном времени
-./xmrig --config=config.json | tee -a $LOGFILE
+    # Запуск XMRig и логирование в реальном времени
+    ./xmrig --config=config.json | tee -a "$LOGFILE"
+}
+
+# Запуск всех функций
+install_dependencies
+install_python_packages
+build_xmrig
+create_config_file
+run_xmrig
